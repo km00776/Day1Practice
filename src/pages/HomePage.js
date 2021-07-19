15a2c3b2a8772838074944edc778927e004b8a96
km00776 <kmurugathas@gmail.com>
2021-07-19 16:40:01 -0700

    background image added@

---
+++ src/pages/HomePage.js
@@ -0,0 +1,18 @@
+import { Component } from 'react';
+import '../styles/HomePage.css';
+
+class HomePage extends Component {
+    // eslint-disable-next-line no-useless-constructor
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        return (
+            <div>
+                hi
+            </div>
+        )
+    }
+}
+
+export default HomePage;
